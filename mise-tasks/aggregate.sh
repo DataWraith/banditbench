007c3c9785b0028156a9d908cfadac1d612a1ff1
Johannes Holzfuß <johannes@holzfuss.name>
2025-03-08 23:22:08 +0100

    tasks: Fix aggregate.sh script

--- mise-tasks/aggregate.sh
+++ mise-tasks/aggregate.sh
@@ -4,6 +4,6 @@
 
 uv run aggregate_ranks.py
 
-echo "| Algorithm | Average Rank | Average Time (seconds) |" > aggregated_ranks.csv
-echo "|---|---|---|" >> aggregated_ranks.csv
-cat aggregated_ranks.csv | sed 's/.*/|&|/' | sed 's/\;/\|/g' >> aggregated_ranks.csv
+echo "| Algorithm | Average Rank | Average Time (seconds) |" > aggregated_ranks.md
+echo "|---|---|---|" >> aggregated_ranks.md
+cat aggregated_ranks.csv | sed 's/.*/|&|/' | sed 's/\;/\|/g' >> aggregated_ranks.md
